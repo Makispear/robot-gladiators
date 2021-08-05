daa79de351b7b3c9bd62142c664857937ac2264b
Maki Aboabida <maki-miko@hotmail.com>
2021-08-04 23:04:23 -0400

    added enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,13 +3,12 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(`playerName: ${playerName}, playerAttack:  ${playerAttack}, playerHealth:  ${playerHealth}`);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -17,53 +16,53 @@ var fight = function() {
     // if player chooses to fight: 
     if (promptFight === "fight" || promptFight === "FIGHT") {
         // enemy gains damage 
-        enemyHealth = enemyHealth - playerAttack; 
-        console.log(
-            `${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} health left.`
-        );
-
+            enemyHealth = enemyHealth - playerAttack; 
+            console.log(
+                `${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} health left.`
+            );
         // checking enemy's health 
-        if (enemyHealth <= 0) {
-            window.alert(`${enemyName} has died!`);
-        } else {
-            window.alert(`${enemyName} still has ${enemyHealth} health left.`)
-        }
-
+            if (enemyHealth <= 0) {
+                window.alert(`${enemyName} has died!`);
+            } else {
+                window.alert(`${enemyName} still has ${enemyHealth} health left.`)
+            }
         // user gains damage 
-        playerHealth = playerHealth - enemyAttack; 
-        console.log(`${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health left.`);
-
+            playerHealth = playerHealth - enemyAttack; 
+            console.log(`${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health left.`);
         // checking player's health 
-        if (playerHealth <=0) {
-            window.alert(`${playerName} has died!`);
-        } else {
-            window.alert(`${playerName} still has ${playerHealth} health left.`)
-        };
-
+            if (playerHealth <=0) {
+                window.alert(`${playerName} has died!`);
+            } else {
+                window.alert(`${playerName} still has ${playerHealth} health left.`)
+            };
         // if player chooses to skip  
-
     } else if (promptFight === "skip" || promptFight === "SKIP") {
         // confirming player wants to skip 
-        var confirmSkip = window.confirm("Are you sure you want to quit?")
-
+            var confirmSkip = window.confirm("Are you sure you want to quit?")
         // if yes leave 
-        if (confirmSkip) {
-            window.alert(`${playerName} has decided to skip this fight. Goodbye!`);
-
+            if (confirmSkip) {
+                window.alert(`${playerName} has decided to skip this fight. Goodbye!`);
         // subtract money form player for skipping 
-            playerMoney = playerMoney - 2;
-        }
+                playerMoney = playerMoney - 2;
+            }
+            window.alert(`${playerName} has chosen to skip the fight!`);
+        } else {
+            window.alert(`You need to choose a valid option. Try again!`)
+        };
 
-        
+        // GAME STATES 
+        // "WIN" - Player robot has defeated all enemy-robots 
+            // *FIght all enemy robots
+            // *Defeat each enemy-robot 
+        // "LOSE" - Player robot's health is zero or less 
+};
+// fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
 
-
-        window.alert(`${playerName} has chosen to skip the fight!`);
-    } else {
-        window.alert(`You need to choose a valid option. Try again!`)
-    }
-
-
-    
-  };
-
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyName[i] + " is at " + i + " index");
+  }
