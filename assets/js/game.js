73b0adfbf6e2b639267561c1a6e717b8a5914be9
Maki Aboabida <maki-miko@hotmail.com>
2021-08-05 23:46:21 -0400

    Added game restart option via restartGame()

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,8 +8,7 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-        // Welcome screen 
-        window.alert("Welcome to Robot Gladiators!");
+
 
 var fight = function(enemyName) {
     // repeat and execute as long as the enemy-robot is alive 
@@ -54,21 +53,54 @@ var fight = function(enemyName) {
                     window.alert(`${playerName} still has ${playerHealth} health left.`)
                 }
     }
-            // GAME STATES 
-            // "WIN" - Player robot has defeated all enemy-robots 
-                // *FIght all enemy robots
-                // *Defeat each enemy-robot 
-            // "LOSE" - Player robot's health is zero or less
+};
+        // function to start a new game 
+var startGame = function() {
+    // debugger;
+                // reset player stats 
+        playerHealth = 100;
+        playerAttack = 10;
+        playerMoney = 10;
+    for(var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            window.alert(`Welcome To Robot Gladiators! Round (${i + 1})`);
+            var pickedEnemyName  = enemyNames[i];
+                // Reset enemy health 
+            enemyHealth = 50;
+            // debugger;
+                // FIGHT 
+            fight(pickedEnemyName);
+            } else {
+            window.alert(`You have lost your robot in battle! Game over!`);
+            break;
+            };
+    };
+    endGame()
 };
 
-for(var i = 0; i < enemyNames.length; i++) {
-        // debugger;
-    // picking enemy name 
-        var pickedEnemyName  = enemyNames[i];
-    // resets health of enemy before proceeding 
-        enemyHealth = 50;
-    // call fight function with enemy-robot 
-        fight(enemyNames[i]);}
+var endGame = function() {
+    // if player is alive they win 
+    if (playerHealth > 0) {
+        window.alert(`Great job, you've survived the game! You now have a score of ${playerMoney}.`)
+        } else {
+            window.alert("You've lost your robot in battle")
+        };
+        // ask player if they want to play again 
+    var playAgainConfirm = window.confirm("would you like to play again?");
+
+    if (playAgainConfirm) {
+        // restart 
+        startGame()
+        } else {
+        window.alert("Thank you for playing Root GLadiators! come back soon!");
+        };
+
+    }
+
+startGame();
+
+
+
                                             
 
 // for(var i = 0; i < enemyNames.length; i++) {
